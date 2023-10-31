309f56c186e2589f769dbbe16a8398c28b474bc5
Willy2874 <willymutungi.k@gmail.com>
2023-10-31 20:44:37 +0300

    python files

---
+++ control_structures/index.py
@@ -0,0 +1,25 @@
+#if it's lunch time 
+    #eat lunch
+#or 
+    #no lunch
+
+#the comparissons to use
+# ==
+# !=
+# >
+# >=
+# <
+# <=
+# or
+# and
+
+event = 'breakfast'
+
+if event == 'Lunch':
+    print('Eat Lunch') #condition
+elif event == 'Supper':
+    print('Eat Suppper')
+elif event == 'breakfast':
+    print ('Breakfast')
+else:
+    print('No Lunch or Supper')   
